1d61ef0bdc3120e49b1ad4a4acbe40800ce4627a
ucs-portal <ucssite2021@gmail.com>
2021-04-20 14:14:57 -0600

    social footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -74,7 +74,7 @@ const Footer = class extends React.Component {
                 </section>
               </div>
               <div className="column is-4 social">
-                <a title="facebook" href="https://facebook.com">
+                <a title="facebook" href="https://www.facebook.com/utahcontractsewing/">
                   <img
                     src={facebook}
                     alt="Facebook"
@@ -89,17 +89,10 @@ const Footer = class extends React.Component {
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
-                <a title="instagram" href="https://instagram.com">
+                <a title="instagram" href="https://www.instagram.com/utahcontractsewing/">
                   <img
                     src={instagram}
                     alt="Instagram"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="vimeo" href="https://vimeo.com">
-                  <img
-                    src={vimeo}
-                    alt="Vimeo"
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
